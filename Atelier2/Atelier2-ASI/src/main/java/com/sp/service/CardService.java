ace4797847bc3fea3c81292229ad2a7f4564d5a2
Guillaume Laville <guillaume.laville@tpc11.cpe.lan>
2022-05-12 17:29:22 +0200

    card controller and service

---
+++ Atelier2/Atelier2-ASI/src/main/java/com/sp/service/CardService.java
@@ -0,0 +1,32 @@
+package com.sp.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sp.entity.Card;
+
+@Service
+public class CardService {
+	
+
+	public int addCard(Card card) {
+		//TODO
+		int id = 0;
+		return id;
+	}
+	
+	public List<Card> getCards(int id) {
+		List<Card> cards = new ArrayList<Card>();
+		return cards;
+	}
+
+	public void updateCard(Card card) {
+		//TODO
+	}
+	
+	public void deleteCard(Card card) {
+		//TODO
+	}
+}
