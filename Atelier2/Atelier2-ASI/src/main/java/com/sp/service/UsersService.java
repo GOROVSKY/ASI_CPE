605a40aff7f81bc87bd1db5d76f54d38f074ca06
pierre <pierre.khettal@etu.univ-lyon1.fr>
2022-05-30 11:20:59 +0200

    donne 5 cartes à l'inscription

--- Atelier2/Atelier2-ASI/src/main/java/com/sp/service/UsersService.java
+++ Atelier2/Atelier2-ASI/src/main/java/com/sp/service/UsersService.java
@@ -5,9 +5,15 @@ package com.sp.service;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
+import java.util.Random;
 
 import com.sp.authentification.config.JwtTokenUtil;
+import com.sp.entity.Card;
+import com.sp.entity.UserCard;
+import com.sp.entity.UserCardId;
 import com.sp.entity.Users;
+import com.sp.model.CardRepository;
+import com.sp.model.UserCardRepository;
 import com.sp.model.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +25,13 @@ public class UsersService {
 
 	@Autowired
     private UserRepository userRepository;
+	
+	@Autowired
+    private CardRepository cardRepository;
 
+	@Autowired
+    private UserCardRepository userCardRepository;
+	
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 	
@@ -40,7 +52,29 @@ public class UsersService {
 	public Users addUser( Users u) {
 		u.setPassword(passwordEncoder.encode(u.getPassword()));
 		u.setWallet(5000);
+		
+		List<Card> result = 
+				  StreamSupport.stream(cardRepository.findAll().spliterator(), false)
+				  .collect(Collectors.toList());
+		
 		userRepository.save(u);
+		
+		Random random = new Random();
+		for(int i=0;i<5;i++)
+		{
+			int value = random.nextInt(result.size());
+			Card c = result.get(value);			
+			UserCard uc = userCardRepository.findByIdCardIdAndIdUserId(c.getId(),u.getId());
+			if(uc == null)
+			{
+				uc = new UserCard(new UserCardId(c.getId(),u.getId()),1);
+			}
+			else {
+				uc.setQuantity(uc.getQuantity()+1);
+			}
+			userCardRepository.save(uc);
+		}
+		
 		return u;
 	}
 	
