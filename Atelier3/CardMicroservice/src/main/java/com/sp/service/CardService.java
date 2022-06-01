eb1b30c5145596e0b2ee24aa585fbfd47b9fadeb
thomas-vargiolu <71138040+thomas-vargiolu@users.noreply.github.com>
2022-06-01 09:45:09 +0200

    Add files via upload

--- Atelier3/CardMicroservice/src/main/java/com/sp/service/CardService.java
+++ Atelier3/CardMicroservice/src/main/java/com/sp/service/CardService.java
@@ -4,19 +4,33 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import com.sp.dto.CardDTO;
+import com.sp.dto.CardTagDTO;
 import com.sp.entity.Card;
 import com.sp.model.CardRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CardService {
-		
+	private String urlTag = "http://127.0.0.1:5000/api/v1/resources/photo?url="	;
 	@Autowired
 	CardRepository cardRepository ;
+	
+	private final RestTemplate restTemplate;
 
+	public CardService(RestTemplateBuilder restTemplateBuilder) {
+		this.restTemplate = restTemplateBuilder.build();
+	}
 	public void addCard(Card card) {
+		
+		List<String> cardtag = this.restTemplate.getForObject(urlTag + card.getImageUrl(), List.class);
+		cardtag.size();
+		System.out.println(cardtag);
+		System.out.println(cardtag.size());
 		cardRepository.save(card);
 	}
 	
